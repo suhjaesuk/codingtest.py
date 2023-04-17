b8cd404a292182047c60102fd200aad250e106f2
suhjaesuk <suhjaesuk@gmail.com>
2023-04-17 16:40:03 +0900

    이코테 문제풀이

---
+++ thisiscodingtest/P_문자열뒤집기.py
@@ -0,0 +1,16 @@
+n = input()
+count0 = 0
+count1 = 1
+
+if n[0] =='1':
+    count0 += 1
+else:
+    count1 += 1
+
+for i in range(len(n) - 1):
+    if n[i] != n[i + 1]:
+        if n[i + 1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+print(min(count0, count1))
